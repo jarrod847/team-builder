0321a3eb08b2bb9778e4f06a5ab56c11a11e76bf
jarrodskahill <jarrodskahill@gmail.com>
2019-11-14 08:33:08 -0800

    finished team member page

---
+++ team-builder/src/Components/TeamMember.js
@@ -0,0 +1,19 @@
+import React from "react"
+
+const people = props => {
+    return(
+        <div>
+            {props.person.map((e, index) =>{
+                return(
+                    <div key={index}>
+                    <h2>{e.name}</h2>
+                    <p>{e.email}</p>
+                    <p>{e.role}</p>
+                    </div>
+                )
+            })}
+        </div>
+    )
+}
+
+export default people
