cd3c3e99297084c77671c2c62f5777fe3ddfe508
jarrodskahill <jarrodskahill@gmail.com>
2019-11-14 08:04:21 -0800

    started on form by adding a function with useState

---
+++ team-builder/src/Components/Form.js
@@ -0,0 +1,9 @@
+import React, {useState} from "react"
+
+const TeamMemeber = props => {
+    const [memeber, setMemeber] = useState({
+        name:"",
+        email:"",
+        role:""
+    })
+}
